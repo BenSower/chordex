d4c8cdaceceea7b8dda61d00848bf3fe1f8c5af9
yannick-cw <Yannick@Yannicks-MacBook-Pro.fritz.box>
2015-01-18 14:39:47 +0100

    Names Tooltip added
    wenn man auf ein chord element klick, wird nun ein Tooltip mit namen
    angezeigt!

---
+++ scripts/c6f155ed.main.js
@@ -0,0 +1,1 @@
+"use strict";var sampleSVG=d3.select("#viz").append("svg").attr("width",900).attr("height",500);sampleSVG.append("circle").style("stroke","gray").style("fill","white").attr("r",100).attr("cx",400).attr("cy",250).on("mouseover",function(){d3.select(this).style("fill","aliceblue")}).on("mouseout",function(){d3.select(this).style("fill","white")});
